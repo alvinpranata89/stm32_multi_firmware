c70c56847914e355f5e3eec68b685871162a029d
AlvinPranata <alvin.pranata89@yahoo.com>
2025-09-03 14:09:38 +0700

    Add files via upload

---
+++ default_app/Debug/Src/timebase.d
@@ -0,0 +1,18 @@
+Src/timebase.o: ../Src/timebase.c ../Inc/timebase.h \
+ C:/Users/lowz_/STM32CubeIDE/EmbeddedExpertTraining/final_project_app1/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/Users/lowz_/STM32CubeIDE/EmbeddedExpertTraining/final_project_app1/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xe.h \
+ C:/Users/lowz_/STM32CubeIDE/EmbeddedExpertTraining/final_project_app1/chip_headers/CMSIS/Include/core_cm4.h \
+ C:/Users/lowz_/STM32CubeIDE/EmbeddedExpertTraining/final_project_app1/chip_headers/CMSIS/Include/cmsis_version.h \
+ C:/Users/lowz_/STM32CubeIDE/EmbeddedExpertTraining/final_project_app1/chip_headers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/lowz_/STM32CubeIDE/EmbeddedExpertTraining/final_project_app1/chip_headers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/lowz_/STM32CubeIDE/EmbeddedExpertTraining/final_project_app1/chip_headers/CMSIS/Include/mpu_armv7.h \
+ C:/Users/lowz_/STM32CubeIDE/EmbeddedExpertTraining/final_project_app1/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+../Inc/timebase.h:
+C:/Users/lowz_/STM32CubeIDE/EmbeddedExpertTraining/final_project_app1/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+C:/Users/lowz_/STM32CubeIDE/EmbeddedExpertTraining/final_project_app1/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xe.h:
+C:/Users/lowz_/STM32CubeIDE/EmbeddedExpertTraining/final_project_app1/chip_headers/CMSIS/Include/core_cm4.h:
+C:/Users/lowz_/STM32CubeIDE/EmbeddedExpertTraining/final_project_app1/chip_headers/CMSIS/Include/cmsis_version.h:
+C:/Users/lowz_/STM32CubeIDE/EmbeddedExpertTraining/final_project_app1/chip_headers/CMSIS/Include/cmsis_compiler.h:
+C:/Users/lowz_/STM32CubeIDE/EmbeddedExpertTraining/final_project_app1/chip_headers/CMSIS/Include/cmsis_gcc.h:
+C:/Users/lowz_/STM32CubeIDE/EmbeddedExpertTraining/final_project_app1/chip_headers/CMSIS/Include/mpu_armv7.h:
+C:/Users/lowz_/STM32CubeIDE/EmbeddedExpertTraining/final_project_app1/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
